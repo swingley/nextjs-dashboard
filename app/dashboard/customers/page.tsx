59c83eadc9a9ed8f60ad335e722e9a767c279a92
Derek Swingley <swingley@gmail.com>
2024-01-22 11:38:05 -0800

    Use customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,13 @@
 import { Metadata } from 'next';
+import { fetchAllCustomers } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
 
 export const metadata: Metadata = {
   title: 'Customers tk',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page() {
+  const customers = await fetchAllCustomers();
+  console.log('customers', customers);
+  return <Table customers={customers} />;
 }
